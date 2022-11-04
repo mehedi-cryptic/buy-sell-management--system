a1b3be96c8a79a1cc452abeae3512483230cc758
sazzad7299 <sazzadurrahman580@gmail.com>
2022-11-04 23:38:38 +0600

    Update

---
+++ team - Copy.php
@@ -0,0 +1,123 @@
+<?php include "includes/header.php" ?>
+
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="Design/countercss/img/favicon.png" rel="icon">
+  <link href="Design/countercss/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="Design/countercss/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="Design/countercss/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="Design/countercss/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="Design/countercss/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="Design/countercss/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="Design/countercss/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="Design/countercss/css/style.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+
+ <!-- ======= Our Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Our Team</h2>
+          
+		</div>
+
+        <div class="row">
+
+          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="member">
+              <div class="pic"><img src="Design/countercss/img/sakib.jpg" style="height:300px" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Sakibul Islam</h4>
+                <span>Developer</span>
+                <div class="social">
+                  <a href=""><i class="icofont-twitter"></i></a>
+                  <a href=""><i class="icofont-facebook"></i></a>
+                  <a href=""><i class="icofont-instagram"></i></a>
+                  <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="member">
+              <div class="pic"><img src="Design/countercss/img/mehedi.jpg"style="height:300px" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Mehedi Hasan</h4>
+                <span>Developer</span>
+                <div class="social">
+                  <a href=""><i class="icofont-twitter"></i></a>
+                  <a href=""><i class="icofont-facebook"></i></a>
+                  <a href=""><i class="icofont-instagram"></i></a>
+                  <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="member">
+              <div class="pic"><img src="Design/countercss/img/sahin.jpg" style="height:300px" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Md Shahin Sanaous</h4>
+                <span>Developer</span>
+                <div class="social">
+                  <a href=""><i class="icofont-twitter"></i></a>
+                  <a href=""><i class="icofont-facebook"></i></a>
+                  <a href=""><i class="icofont-instagram"></i></a>
+                  <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="member">
+              <div class="pic"><img src="Design/countercss/img/sumiha.jpg" style="height:300px" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Sumiha Jahan</h4>
+                <span>Developer</span>
+                <div class="social">
+                  <a href=""><i class="icofont-twitter"></i></a>
+                  <a href=""><i class="icofont-facebook"></i></a>
+                  <a href=""><i class="icofont-instagram"></i></a>
+                  <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Team Section -->			
+ 
+ 
+  
+</body>
+</html>
+
+
+<?php include "includes/footer.php" ?>	
